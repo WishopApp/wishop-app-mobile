993781232dc1060388e6078657e70e9f50252e86
huag1234@hotmail.com <huag1234@hotmail.com>
2018-03-27 23:31:09 +0700

    [ADD] React ESLint and prettier

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import Navigation from './src/components/screens/Navigation';
 
 class App extends React.Component {
-
-  render() {
-    return (
-      <View style={style.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Test React-native application312321</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={style.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+				<Text>Changes you make will automatically reload.</Text>
+				<Text>Shake your phone to open the developer menu.</Text>
+				<Text>Test React-native application3121</Text>
+				<Navigation />
+			</View>
+		);
+	}
 }
 
 const style = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+	container: {
+		display: 'flex',
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
 
-
-
 export default App;
-
-
---
+++ src/components/screens/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+class Navigation extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>Navigation Text2</Text>
+			</View>
+		);
+	}
+}
+
+export default Navigation;
